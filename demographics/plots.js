abcbd3b877c3f1c57c0c77984419735ced586a47
Colin Brineman <colinbrineman@Colins-MacBook.local>
2022-02-16 17:31:56 -0500

    Initial commit

---
+++ demographics/plots.js
@@ -0,0 +1,33 @@
+function init() {
+    var selector = d3.select("#selDataset");
+
+    d3.json("samples.json").then((data) => {
+        console.log(data);
+        var sampleNames = data.names;
+        sampleNames.forEach((sample) => {
+            selector
+                .append("option")
+                .text(sample)
+                .property("value", sample);
+        });
+    })
+}
+
+init();
+
+function optionChanged(newSample) {
+    buildMetadata(newSample);
+    buildCharts(newSample);
+}
+
+function buildMetadata(sample) {
+    d3.json("samples.json").then((data) => {
+        var metadata = data.metadata;
+        var resultArray = metadata.filter(sampleObj => sampleObj.id == sample);
+        var result = resultArray[0];
+        var PANEL = d3.select("#sample-metadata");
+
+        PANEL.html("");
+        PANEL.append("h6").text(result.location);
+    });
+}
