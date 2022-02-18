029ca0d1a94696c55f0c9e240c8184c2bf208e95
Colin Brineman <colinbrineman@Colins-MacBook.local>
2022-02-17 20:03:20 -0500

    updated panel

--- demographics/plots.js
+++ demographics/plots.js
@@ -17,7 +17,7 @@ init();
 
 function optionChanged(newSample) {
     buildMetadata(newSample);
-    buildCharts(newSample);
+    //    buildCharts(newSample);
 }
 
 function buildMetadata(sample) {
@@ -25,9 +25,15 @@ function buildMetadata(sample) {
         var metadata = data.metadata;
         var resultArray = metadata.filter(sampleObj => sampleObj.id == sample);
         var result = resultArray[0];
+
         var PANEL = d3.select("#sample-metadata");
 
         PANEL.html("");
-        PANEL.append("h6").text(result.location);
+        Object.entries(result).forEach(([key, value]) => {
+            PANEL.append("h6").text(`${key}: ${value}`);
+          });
+
     });
 }
+
+//python -m http.server
